0ce26ff8dfada28a53c7b376626bdddcb1445d49
brendan <brendan.poole@gmail.com>
2016-05-30 09:30:28 +0100

    Tetris

---
+++ Assets/Cardboard/Tetris/Grid.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Grid : MonoBehaviour {
+
+    public static int width = 10;
+    public static int height = 20;
+    public static Transform[,] grid = new Transform[width, height];
+
+    public static Vector2 roundVec2(Vector2 v)
+    {
+        return new Vector2(Mathf.Round(v.x),
+                           Mathf.Round(v.y));
+    }
+
+    public static bool insideBorder(Vector2 pos)
+    {
+        return ((int)pos.x >= 0 &&
+                (int)pos.x < width &&
+                (int)pos.y >= 0);
+    }
+    public static void deleteRow(int y)
+    {
+        for (int x = 0; x < width; ++x)
+        {
+            Destroy(grid[x, y].gameObject);
+            grid[x, y] = null;
+        }
+    }
+    public static void decreaseRow(int y)
+    {
+        for (int x = 0; x < width; ++x)
+        {
+            if (grid[x, y] != null)
+            {
+                // Move one towards bottom
+                grid[x, y - 1] = grid[x, y];
+                grid[x, y] = null;
+
+                // Update Block position
+                grid[x, y - 1].position += new Vector3(0, -1, 0);
+            }
+        }
+    }
+    public static void decreaseRowsAbove(int y)
+    {
+        for (int i = y; i < height; ++i)
+            decreaseRow(i);
+    }
+
+    public static bool isRowFull(int y)
+    {
+        for (int x = 0; x < width; ++x)
+            if (grid[x, y] == null)
+                return false;
+        return true;
+    }
+    public static void deleteFullRows()
+    {
+        for (int y = 0; y < height; ++y)
+        {
+            if (isRowFull(y))
+            {
+                deleteRow(y);
+                decreaseRowsAbove(y + 1);
+                --y;
+            }
+        }
+    }
+    void updateGrid()
+    {
+        // Remove old children from grid
+        for (int y = 0; y < Grid.height; ++y)
+            for (int x = 0; x < Grid.width; ++x)
+                if (Grid.grid[x, y] != null)
+                    if (Grid.grid[x, y].parent == transform)
+                        Grid.grid[x, y] = null;
+
+        // Add new children to grid
+        foreach (Transform child in transform)
+        {
+            Vector2 v = Grid.roundVec2(child.position);
+            Grid.grid[(int)v.x, (int)v.y] = child;
+        }
+    }
+}
---
+++ Assets/Cardboard/Tetris/Group.cs
@@ -0,0 +1,174 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Group : MonoBehaviour {
+    float lastFall = 0;
+    float repeatTime = .2f;
+    float downRepeatTime = .15f;
+    float lastInput = float.MinValue;
+    float startTime = Time.time;
+    float initialDelay = .5f;
+
+    void Start()
+    {
+        // Default position not valid? Then it's game over
+        if (!isValidGridPos())
+        {
+            Debug.Log("GAME OVER");
+            //Destroy(gameObject);
+        }
+    }
+
+    void Update()
+    {
+        if(Time.time - startTime < initialDelay)
+        {
+            return;
+        }
+        float vAxis = Input.GetAxis("Vertical");
+        float hAxis = Input.GetAxis("Horizontal");
+
+        // check if user let go of the stick; if so, reset the input bounce control
+        if (Mathf.Abs(vAxis) < 0.1f && Mathf.Abs(hAxis) < 0.1f) {
+            lastInput = float.MinValue;
+        }
+
+            if (hAxis < -0.1f && Time.time - lastInput > repeatTime)
+            {
+                // Modify position
+                transform.position += new Vector3(-1, 0, 0);
+
+                // See if valid
+                if (isValidGridPos())
+                {
+                    // Its valid. Update grid.
+                    updateGrid();
+                    lastInput = Time.time;
+                }
+
+                else
+                    // Its not valid. revert.
+                    transform.position += new Vector3(1, 0, 0);
+            } // Move Right
+            else if (hAxis > 0.1f && Time.time - lastInput > repeatTime)
+            {
+                // Modify position
+                transform.position += new Vector3(1, 0, 0);
+
+                // See if valid
+                if (isValidGridPos())
+                {
+                    // It's valid. Update grid.
+                    updateGrid();
+                    lastInput = Time.time;
+                }
+                else
+                    // It's not valid. revert.
+                    transform.position += new Vector3(-1, 0, 0);
+            }// Rotate
+        else if (Input.GetButtonDown("Fire1") && Time.time - lastInput > repeatTime)
+        {
+            transform.Rotate(0, 0, -90);
+
+            // See if valid
+            if (isValidGridPos())
+            {
+                // It's valid. Update grid.
+                updateGrid();
+                lastInput = Time.time;
+            }
+            else
+                // It's not valid. revert.
+                transform.Rotate(0, 0, 90);
+        }
+        else if (Input.GetButtonDown("Fire2") && Time.time - lastInput > repeatTime)
+        {
+            transform.Rotate(0, 0, 90);
+
+            // See if valid
+            if (isValidGridPos())
+            {
+                // It's valid. Update grid.
+                updateGrid();
+                lastInput = Time.time;
+            }
+            else
+                // It's not valid. revert.
+                transform.Rotate(0, 0, 90);
+        }
+        else if (vAxis < -0.1f && Time.time - lastInput > downRepeatTime)
+            {
+                moveDown();
+                lastInput = Time.time;
+            }
+            if (Time.time - lastFall >= 1)
+            {
+                moveDown();
+            }
+    }
+    private void moveDown()
+    {
+        // Modify position
+        transform.position += new Vector3(0, -1, 0);
+
+        // See if valid
+        if (isValidGridPos())
+        {
+            // It's valid. Update grid.
+            updateGrid();
+        }
+        else
+        {
+            // It's not valid. revert.
+            transform.position += new Vector3(0, 1, 0);
+
+            // Clear filled horizontal lines
+            Grid.deleteFullRows();
+
+            // Spawn next Group
+            FindObjectOfType<Spawner>().spawnNext();
+
+            // Disable script
+            enabled = false;
+        }
+
+        lastFall = Time.time;
+
+    }
+
+    bool isValidGridPos()
+    {
+        foreach (Transform child in transform)
+        {
+            Vector2 v = Grid.roundVec2(child.position);
+
+            // Not inside Border?
+            if (!Grid.insideBorder(v))
+                return false;
+
+            // Block in grid cell (and not part of same group)?
+            if (Grid.grid[(int)v.x, (int)v.y] != null &&
+                Grid.grid[(int)v.x, (int)v.y].parent != transform)
+                return false;
+        }
+        return true;
+    }
+
+    void updateGrid()
+    {
+        // Remove old children from grid
+        for (int y = 0; y < Grid.height; ++y)
+            for (int x = 0; x < Grid.width; ++x)
+                if (Grid.grid[x, y] != null)
+                    if (Grid.grid[x, y].parent == transform)
+                        Grid.grid[x, y] = null;
+
+        // Add new children to grid
+        foreach (Transform child in transform)
+        {
+            Vector2 v = Grid.roundVec2(child.position);
+            Grid.grid[(int)v.x, (int)v.y] = child;
+        }
+    }
+
+}
